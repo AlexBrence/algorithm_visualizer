e909b22c76aadc85b9a06cc63515528f703ff348
Alex Brence <alexbrence@protonmail.com>
2025-04-21 12:01:54 +0200

    adds quick sort algorithm

--- src/algorithms.rs
+++ src/algorithms.rs
@@ -1,8 +1,8 @@
 use core::fmt;
-use macroquad::{prelude::*, ui::{hash, root_ui, widgets}};
+use macroquad::prelude::*;
 
 
-const BAR_WIDTH: f32 = 30.0;
+pub const BAR_WIDTH: f32 = 30.0;
 
 #[derive(Clone, Copy, Debug, PartialEq, Default)]
 #[non_exhaustive]
@@ -31,12 +31,18 @@ pub struct Algorithm {
 }
 
 impl Algorithm {
-    pub async fn do_magic(&self, values: &mut Vec<f32>) {
+    pub async fn do_magic(&self, values: &mut [i32]) {
+        if values.is_empty() {
+            return;
+        }
+
         match &self.current_type {
             Type::BubbleSort => {
                 bubble_sort(values, self.slowdown).await;
             },
-            Type::QuickSort => todo!(),
+            Type::QuickSort => {
+                quick_sort(values, 0, values.len() - 1, self.slowdown).await;
+            }
             Type::SelectionSort => todo!(),
             Type::InsertionSort => todo!(),
             Type::MergeSort => todo!(),
@@ -56,9 +62,10 @@ impl Default for Algorithm {
     }
 }
 
-fn draw_bars(values: &[f32], highlighted1: usize, highlighted2: usize) {
+pub fn draw_bars(values: &[i32], highlighted1: usize, highlighted2: usize) {
     for (i, &value) in values.iter().enumerate() {
         let color = if i == highlighted1 || i == highlighted2 { RED } else { BLUE };
+
         let bar_width = if values.len() > 150 {
             BAR_WIDTH / 2.5
         } else if values.len() > 80 {
@@ -67,23 +74,62 @@ fn draw_bars(values: &[f32], highlighted1: usize, highlighted2: usize) {
             BAR_WIDTH
         };
         
-        draw_rectangle(i as f32 * bar_width, screen_height() - value, bar_width - 5.0, value, color);
+        draw_rectangle(i as f32 * bar_width, screen_height() - value as f32, bar_width - 5.0, value as f32, color);
     }
 }
 
-async fn bubble_sort(values: &mut Vec<f32>, slowdown: f32) {
+async fn redraw_and_slowdown(values: &[i32], highlighted1: usize, highlighted2: usize, slowdown: f32) {
+    clear_background(BLACK);
+    draw_bars(values, highlighted1, highlighted2);
+    next_frame().await; // This keeps the loop running
+    std::thread::sleep(std::time::Duration::from_secs_f32(slowdown)); // Slows down sorting for visibility
+}
+
+// ========== Bubble sort ==========
+async fn bubble_sort(values: &mut [i32], slowdown: f32) {
     let len = values.len();
     for i in 0..len {
         for j in 0..len - i - 1 {
             if values[j] > values[j + 1] {
                 values.swap(j, j + 1);
 
-                // Clear screen and redraw bars
-                clear_background(BLACK);
-                draw_bars(values, j, j + 1);
-                next_frame().await; // This keeps the loop running
-                std::thread::sleep(std::time::Duration::from_secs_f32(slowdown)); // Slows down sorting for visibility
+                // Redraw bars
+                redraw_and_slowdown(values, j, j + 1, slowdown).await;
             }
         }
+    }
+}
+
+
+/*
+ * Quick sort with Lomuto partition
+ * https://www.geeksforgeeks.org/quick-sort-algorithm/
+ */
+
+async fn partition(values: &mut [i32], low: usize, high: usize, slowdown: f32) -> usize {
+    let pivot = values[high];
+    let mut i = low;
+
+    for j in low..high {
+        if values[j] < pivot {
+            values.swap(i, j);
+            redraw_and_slowdown(values, j, i, slowdown).await;
+            i += 1;
+        }
+    }
+    values.swap(i, high);
+    redraw_and_slowdown(values, i, high, slowdown).await;
+
+    i
+}
+
+async fn quick_sort(values: &mut [i32], low: usize, high: usize, slowdown: f32) {
+    if low < high {
+        let pivot_idx = partition(values, low, high, slowdown).await;
+
+        if pivot_idx > 0 {
+            Box::pin(quick_sort(values, low, pivot_idx - 1, slowdown)).await;
+        }
+        Box::pin(quick_sort(values, pivot_idx + 1, high, slowdown)).await;
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,65 +1,12 @@
 mod algorithms;
+use algorithms::draw_bars;
 use macroquad::{prelude::*, ui::{hash, root_ui, widgets}};
-
-const BAR_WIDTH: f32 = 30.0;
-
-// async fn bubble_sort(values: &mut Vec<f32>, slowdown: f32) {
-//     let len = values.len();
-//     for i in 0..len {
-//         for j in 0..len - i - 1 {
-//             if values[j] > values[j + 1] {
-//                 values.swap(j, j + 1);
-//
-//                 // Clear screen and redraw bars
-//                 clear_background(BLACK);
-//                 draw_bars(values, j, j + 1);
-//                 next_frame().await; // This keeps the loop running
-//                 std::thread::sleep(std::time::Duration::from_secs_f32(slowdown)); // Slows down sorting for visibility
-//             }
-//         }
-//     }
-// }
-
-fn draw_bars(values: &[f32], highlighted1: usize, highlighted2: usize) {
-    for (i, &value) in values.iter().enumerate() {
-        let color = if i == highlighted1 || i == highlighted2 { RED } else { BLUE };
-        let bar_width = if values.len() > 150 {
-            BAR_WIDTH / 2.5
-        } else if values.len() > 80 {
-            BAR_WIDTH / 2.0
-        } else {
-            BAR_WIDTH
-        };
-        
-        draw_rectangle(i as f32 * bar_width, screen_height() - value, bar_width - 5.0, value, color);
-    }
-}
-
-// #[derive(Clone, Copy, Debug, PartialEq, Default)]
-// #[non_exhaustive]
-// enum Algorithm {
-//     #[default] 
-//     BubbleSort,
-//     SelectionSort,
-//     InsertionSort,
-//     MergeSort,
-//     HeapSort,
-//     QuickSort,
-//     CountingSort,
-//     RadixSort,
-// }
-//
-// impl fmt::Display for Algorithm {
-//     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-//         write!(f, "{:?}", self)
-//     }
-// }
 
 
 #[macroquad::main("Algorithm Visualizer")]
 async fn main() {
     let mut number_of_bars: f32 = 40.0;
-    let mut values: Vec<f32> = (1..=number_of_bars as u32).map(|_| rand::gen_range(50.0, 400.0)).collect();
+    let mut values: Vec<i32> = (1..=number_of_bars as u32).map(|_| rand::gen_range(50, 400)).collect();
     let mut algo_duration: Option<f64> = None; 
     let mut algorithm: algorithms::Algorithm = Default::default();
 
@@ -97,7 +44,7 @@ async fn main() {
                 });
             });
 
-        draw_bars(&values, usize::MAX, usize::MAX);
+        algorithms::draw_bars(&values, usize::MAX, usize::MAX);
 
         // Run button action
         if widgets::Button::new("Run")
@@ -106,8 +53,10 @@ async fn main() {
             .ui(&mut root_ui())
         {
             let start_time = std::time::Instant::now();
+            println!("Going in with: {:?}", values);
             algorithm.do_magic(&mut values).await;
             algo_duration = Some(start_time.elapsed().as_secs_f64());
+            // assert!(values.is_sorted(), "Not sorted: {:?}", values);
         }
 
         // Reset button action
@@ -116,7 +65,8 @@ async fn main() {
             .position(vec2(screen_width() - 160.0, 30.0))
             .ui(&mut root_ui())
         {
-            values = (1..=number_of_bars as u32).map(|_| rand::gen_range(50.0, 400.0)).collect();
+            values = (1..=number_of_bars as u32).map(|_| rand::gen_range(50, 400)).collect();
+            println!("New values: {:?}", values);
         }
 
         // Show time elapsed
